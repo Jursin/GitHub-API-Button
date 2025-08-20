2b01028e58986cbcadb96305eff7481a54e3b50e
Jursin <1836059252@qq.com>
2025-08-20 09:03:41 +0800

    区分仓库和用户主页

--- main.js
+++ main.js
@@ -12,15 +12,24 @@
 // @supportURL   https://github.com/Jursin/GitHub-API-Button/issues
 // ==/UserScript==
 
-(function () {
+(function() {
     'use strict';
 
     const navBar = document.querySelector('.UnderlineNav-body.list-style-none');
 
     if (navBar) {
-      const repoPath = window.location.pathname.split('/').slice(1, 3).join('/');
+      const pathParts = window.location.pathname.split('/').filter(Boolean);
+      let apiUrl = '';
+      
+      if (pathParts.length === 1) {
+        // 用户页面: /username
+        apiUrl = `https://api.github.com/users/${pathParts[0]}`;
+      } else if (pathParts.length >= 2) {
+        // 仓库页面: /owner/repo
+        apiUrl = `https://api.github.com/repos/${pathParts[0]}/${pathParts[1]}`;
+      }
 
-      if (repoPath) {
+      if (apiUrl) {
         const svgIcon = `
           <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-microchip UnderlineNav-octicon d-none d-sm-inline">
           <path d="M5.5,.75c0-.42-.33-.75-.75-.75s-.75,.33-.75,.75v1.25c-1.1,0-2,.9-2,2H.75c-.42,0-.75,.33-.75,.75s.33,.75,.75,.75h1.25v1.75H.75c-.42,0-.75,.33-.75,.75s.33,.75,.75,.75h1.25v1.75H.75c-.42,0-.75,.33-.75,.75s.33,.75,.75,.75h1.25c0,1.1,.9,2,2,2v1.25c0,.42,.33,.75,.75,.75s.75-.33,.75-.75v-1.25h1.75v1.25c0,.42,.33,.75,.75,.75s.75-.33,.75-.75v-1.25h1.75v1.25c0,.42,.33,.75,.75,.75s.75-.33,.75-.75v-1.25c1.1,0,2-.9,2-2h1.25c.42,0,.75-.33,.75-.75s-.33-.75-.75-.75h-1.25v-1.75h1.25c.42,0,.75-.33,.75-.75s-.33-.75-.75-.75h-1.25v-1.75h1.25c.42,0,.75-.33,.75-.75s-.33-.75-.75-.75h-1.25c0-1.1-.9-2-2-2V.75c0-.42-.33-.75-.75-.75s-.75,.33-.75,.75v1.25h-1.75V.75c0-.42-.33-.75-.75-.75s-.75,.33-.75,.75v1.25h-1.75V.75Zm-.5,3.25h6c.55,0,1,.45,1,1v6c0,.55-.45,1-1,1H5c-.55,0-1-.45-1-1V5c0-.55,.45-1,1-1Zm.5,1.5v5h5V5.5H5.5Z"/>
@@ -33,7 +42,7 @@
 
         const newAnchor = document.createElement('a');
         newAnchor.setAttribute('id', 'api-tab');
-        newAnchor.setAttribute('href', `https://api.github.com/repos/${repoPath}`);
+        newAnchor.setAttribute('href', apiUrl);
         newAnchor.setAttribute('target', '_blank');
         newAnchor.className = 'UnderlineNav-item no-wrap js-responsive-underlinenav-item';
 
@@ -45,7 +54,7 @@
 
         console.log('API button added successfully.');
       } else {
-        console.log('Repository path not found.');
+        console.log('API URL could not be determined.');
       }
     } else {
       console.log('Navigation bar not found.');
